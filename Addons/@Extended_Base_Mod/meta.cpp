925689e7728b0bd5064f3c149ea995a63fa7e79a
Damon Smithies <damon.smithies@googlemail.com>
2017-06-14 19:54:10 +0100

    Extended_Base_Mod Update
    Added the new extended base mod and archived the old one.

--- Addons/@Extended_Base_Mod/meta.cpp
+++ Addons/@Extended_Base_Mod/meta.cpp
@@ -1,4 +1,4 @@
 protocol = 1;
 publishedid = 647753401;
 name = "Extended_Base_Mod";
-timestamp = 5247996853917019224;
+timestamp = 5248016157151997421;
--- Addons/@Extended_Base_Mod/mod.cpp
+++ Addons/@Extended_Base_Mod/mod.cpp
@@ -1,5 +1,5 @@
 name = "Extended Base Mod";
 actionName = "EBM";
-version = "0.3.1";
+version = "0.3.3";
 picture = "extendedbase.paa";
 author = "Freakylein";
